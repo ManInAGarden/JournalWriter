2c935a1034c4ed330e4cfb17b371e1a6993b7a8e
ManInAGarden <hspeich@versanet.de>
2016-03-15 18:27:07 +0100

    new debug view added

---
+++ JournalWriter/ShowDebugInfoWindow.xaml.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace JournalWriter
+{
+    /// <summary>
+    /// Interaktionslogik für ShowDebugInfoWindow.xaml
+    /// </summary>
+    public partial class ShowDebugInfoWindow : Window
+    {
+        public ShowDebugInfoWindow()
+        {
+            InitializeComponent();
+        }
+
+        internal void Show(MainWindow mainWindow, FlowDocument fdoc)
+        {
+            this.Owner = mainWindow;
+
+            flowdoDebugFDV.Document = fdoc;
+            Show();
+        }
+    }
+}
